8e5c216713de412220c05d277dc7318827b7a555
FoNiX <rogueman2018@gmail.com>
2024-07-23 22:46:15 +0100

    Adding Task 1 FIFO Cache

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+""" FIFOCache module """
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = self.queue.pop(0)
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
