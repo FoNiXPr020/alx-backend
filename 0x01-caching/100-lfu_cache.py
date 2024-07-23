7330196c01b72521fadc06a1e3da0c29e2136d7d
FoNiX <rogueman2018@gmail.com>
2024-07-23 22:51:30 +0100

    Updating Task 100 LFU Cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,5 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ LFUCache module """
+
 
 from base_caching import BaseCaching
 
@@ -8,29 +9,39 @@ class LFUCache(BaseCaching):
     """ LFUCache class """
 
     def __init__(self):
-        """ Constructor """
+        """ Initialize LFUCache """
         super().__init__()
-        self.lfu = []
+        self.freq_counter = {}
 
     def put(self, key, item):
-        """ Adds an item in the cache """
+        """ Add an item in the cache """
         if key is not None and item is not None:
             if key in self.cache_data:
                 self.cache_data[key] = item
-                self.lfu.remove(key)
+                self.freq_counter[key] += 1
             else:
                 if len(self.cache_data) >= self.MAX_ITEMS:
-                    discard = self.lfu.pop(0)
-                    del self.cache_data[discard]
-                    print("DISCARD: {}".format(discard))
-            self.cache_data[key] = item
-            self.lfu.append(key)
-        self.lfu.sort(key=lambda k: self.cache_data[k])
+                    min_freq = min(self.freq_counter.values())
+                    keys_to_discard = [
+                        k for k, v in self.freq_counter.items()
+                        if v == min_freq]
+                    if len(keys_to_discard) > 1:
+                        lru_key = min(
+                            self.freq_counter,
+                            key=lambda k: self.freq_counter[k])
+                        keys_to_discard = [lru_key]
+                    for k in keys_to_discard:
+                        del self.cache_data[k]
+                        del self.freq_counter[k]
+                        print("DISCARD: {}".format(k))
+                self.cache_data[key] = item
+                self.freq_counter[key] = 1
 
     def get(self, key):
         """ Get an item by key """
-        if key is not None and key in self.cache_data:
-            self.lfu.remove(key)
-            self.lfu.append(key)
-            return self.cache_data[key]
-        return None
+        if key is not None:
+            if key in self.cache_data:
+                self.freq_counter[key] += 1
+                return self.cache_data[key]
+            else:
+                return None
